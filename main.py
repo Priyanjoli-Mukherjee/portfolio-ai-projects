002384951320dfe55ccc4f94eeaf3f5ddcf39024
Priya Mukherjee <priyanjolim@gmail.com>
2026-06-29 17:40:42 -0700

    added env files

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+# Load the values from the .env file into the environment
+load_dotenv()
+
+# Access the hidden variables
+secret_key = os.getenv("API_SECRET_KEY")
+
+print(f"Using API key: {secret_key}")
