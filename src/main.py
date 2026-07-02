bd74f29ac67ac3c554ddb67c0b8f7abba8d2d23c
Priya Mukherjee <priyanjolim@gmail.com>
2026-07-01 23:24:42 -0700

    Add prompt template

--- src/main.py
+++ src/main.py
@@ -28,3 +28,22 @@ query = 'what are the top risks mentioned in the document?'
 docs_chroma = db_chroma.similarity_search_with_score(query, k=5)
 
 context_text = "\n\n".join([doc.page_content for doc, _score in docs_chroma])
+
+from langchain.prompts import ChatPromptTemplate
+from langchain.chat_models import ChatOpenAI
+
+PROMPT_TEMPLATE = """
+Answer the question based only on the following context:
+{context}
+Answer the question based on the above context: {question}.
+Provide a detailed answer.
+Don’t justify your answers.
+Don’t give information not mentioned in the CONTEXT INFORMATION.
+Do not say "according to the context" or "mentioned in the context" or similar.
+"""
+
+prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+prompt = prompt_template.format(context=context_text, question=query)
+
+model = ChatOpenAI()
+response_text = model.predict(prompt)
