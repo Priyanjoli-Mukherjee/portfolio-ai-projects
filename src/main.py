c64609d42649832c491342d52437474600d07602
Priya Mukherjee <priyanjolim@gmail.com>
2026-07-01 21:59:54 -0700

    Implement document chunking

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import os
+from langchain_community.document_loaders import PyPDFLoader
 from dotenv import load_dotenv
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 # Load the values from the .env file into the environment
 load_dotenv()
@@ -11,3 +13,6 @@ print(f"Using API key: {secret_key}")
 
 loader = PyPDFLoader(DOC_PATH)
 pages = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+chunks = text_splitter.split_documents(pages)
